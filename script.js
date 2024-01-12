018a85c07ae32e38bd691ad0db1a7d63c2b195b3
EBok1 <144004887+EBok1@users.noreply.github.com>
2024-01-12 10:53:28 +0100

    .

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-// const menuButton = document.querySelector("#menu"),
-//       navLinks = document.querySelector(".nav-links");
-
-// menuButton.addEventListener("click", toggleMenu)
-// function toggleMenu () {
-//     navLinks.classList.add("visibility") 
-// }
-
-
 let menuButton = document.querySelector('#menu')
-navLinks.addEventListener('.nav-links', toggleMenu)
+menuButton.addEventListener('click', toggleMenu)
 function toggleMenu() { 
-  navLinks.classList.toggle('visibility')
+  menuButton.classList.toggle('visibility')
 }
