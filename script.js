ed1cb1b2d493bce645cb9b9d92536d7325c8d11e
EBok1 <144004887+EBok1@users.noreply.github.com>
2024-01-23 13:47:37 +0100

    .

--- script.js
+++ script.js
@@ -3,20 +3,21 @@ const menuButton = document.querySelector('#menu');
 const modal = document.querySelector('.modal');
 
 menuButton.addEventListener('click', toggleMenu)
-function toggleMenu () {
-  modal.classList.toggle('show')
+function toggleMenu() {
+  modal.classList.toggle('show');
+  document.querySelector('header').classList.toggle('modal-open');
 }
 
 // close button in navigation
 const closeModalButton = document.querySelector('#close-modal');
 
 closeModalButton.addEventListener('click', closeMenu)
-function closeMenu () {
+function closeMenu() {
   modal.classList.remove('show')
 }
 
-const textButton = document.querySelector('#text-height'); 
+const textButton = document.querySelector('#text-height');
 textButton.addEventListener('click', toggleText)
-function toggleText () {
+function toggleText() {
   textButton.classList.toggle('present')
 }
